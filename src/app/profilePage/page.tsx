363350a294e4c2ac1f3f283c57d60887cdd68dde
drh-14 <darrenh1134@gmail.com>
2025-05-26 17:03:16 -0400

    added recommend api route

--- src/app/profilePage/page.tsx
+++ src/app/profilePage/page.tsx
@@ -1,9 +1,15 @@
 'use client'
 import Navbar from '../components/navbar';
+import { Button } from '@mui/material';
 export default function ProfilePage(){
     return(
-        <div>
+        <div className = 'flex flex-col gap-16 items-center'>
             <Navbar></Navbar>
+            <h1 className = 'text-5xl'>Your preferences</h1>
+            <div className = 'flex flex-col items-center gap-8'>
+                <h1 className = 'text-4xl'>Your Linked MyAnimeList Profile</h1>
+                <Button variant = 'contained'>Change Linked Profile</Button>
+            </div>
         </div>
     )
 }
